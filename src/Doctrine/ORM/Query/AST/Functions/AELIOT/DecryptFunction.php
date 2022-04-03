268741d0643df77e2b67e0dbc07b5b29f1d3de94
Anatoliy Melnikov <5785276@gmail.com>
2022-04-03 13:24:49 +0300

    Refactor function AST definitions

--- src/Doctrine/ORM/Query/AST/Functions/AELIOT/DecryptFunction.php
+++ src/Doctrine/ORM/Query/AST/Functions/AELIOT/DecryptFunction.php
@@ -5,35 +5,11 @@ declare(strict_types=1);
 namespace Aeliot\Bundle\DoctrineEncryptedField\Doctrine\ORM\Query\AST\Functions\AELIOT;
 
 use Aeliot\Bundle\DoctrineEncryptedField\Enum\FunctionEnum;
-use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\AST\SimpleArithmeticExpression;
-use Doctrine\ORM\Query\Lexer;
-use Doctrine\ORM\Query\Parser;
-use Doctrine\ORM\Query\SqlWalker;
 
 /**
  * "AELIOT_DECRYPT" "(" SimpleArithmeticExpression ")"
  */
-class DecryptFunction extends FunctionNode
+final class DecryptFunction extends AbstractSingleArgumentFunction
 {
-    public ?SimpleArithmeticExpression $simpleArithmeticExpression = null;
-
-    public function getSql(SqlWalker $sqlWalker): string
-    {
-        return sprintf(
-            '%s(%s)',
-            FunctionEnum::FUNCTION_DECRYPT,
-            $sqlWalker->walkSimpleArithmeticExpression($this->simpleArithmeticExpression)
-        );
-    }
-
-    public function parse(Parser $parser): void
-    {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-
-        $this->simpleArithmeticExpression = $parser->SimpleArithmeticExpression();
-
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
-    }
+    protected const FUNCTION_NAME = FunctionEnum::FUNCTION_DECRYPT;
 }
